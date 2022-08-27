5c02bc0bc889a92d1276a198b098895831610ce2
soluxsolux <dragon2000a@naver.com>
2022-08-27 10:02:59 +0900

    commit

---
+++ src/main/java/com/joobging/joobging/JoobgingApplication.java
@@ -0,0 +1,13 @@
+package com.joobging.joobging;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JoobgingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JoobgingApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/joobging/joobging/JoobgingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.joobging.joobging;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JoobgingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
