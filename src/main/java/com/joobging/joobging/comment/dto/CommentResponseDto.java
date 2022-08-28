ca055e523b22093c3c5a0f414125bde796b0cd79
soluxsolux <dragon2000a@naver.com>
2022-08-28 17:24:06 +0900

    [fix] comment관련 덮어짐 해결

--- src/main/java/com/joobging/joobging/comment/dto/CommentResponseDto.java
+++ src/main/java/com/joobging/joobging/comment/dto/CommentResponseDto.java
@@ -1,12 +1,28 @@
 package com.joobging.joobging.comment.dto;
 
+import com.joobging.joobging.comment.domain.Comment;
+import jdk.nashorn.api.tree.CompilationUnitTree;
 import lombok.Getter;
 import lombok.Setter;
+
+import java.util.List;
+import java.util.stream.Collectors;
 
 @Getter
 @Setter
 public class CommentResponseDto {
 
 
+    private String name;
+    private String mbti;
+    private String content;
+    private List<CommentResponseDto> commentResponseDtoList;
+
+    public CommentResponseDto(Comment comment){
+        this.name = comment.getMember().getName();
+        this.mbti = comment.getMember().getMbti();
+        this.content = comment.getContent();
+        this.commentResponseDtoList = comment.getChildren().stream().map(CommentResponseDto::new).collect(Collectors.toList());
+    }
 
 }
--- src/main/java/com/joobging/joobging/comment/repository/CommentRepository.java
+++ src/main/java/com/joobging/joobging/comment/repository/CommentRepository.java
@@ -2,12 +2,17 @@ package com.joobging.joobging.comment.repository;
 
 import com.joobging.joobging.comment.domain.Comment;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
 import java.util.Optional;
 
 @Repository
 public interface CommentRepository extends JpaRepository<Comment, Long> {
     @Override
     Optional<Comment> findById(Long aLong);
+
+    @Query(value = "select c from Comment c where c.board.id = :id and c.parent IS NULL")
+    List<Comment> findByComment_IdOrderByCreatedDateDesc(Long id);
 }
