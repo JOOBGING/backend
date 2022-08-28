5e9201db72ec7f772d877d1e4166497866310f65
soluxsolux <dragon2000a@naver.com>
2022-08-28 10:12:51 +0900

    [fix] LocationController 수정

--- src/main/java/com/joobging/joobging/location/controller/LocationController.java
+++ src/main/java/com/joobging/joobging/location/controller/LocationController.java
@@ -1,7 +1,31 @@
 package com.joobging.joobging.location.controller;
 
+import com.joobging.joobging.location.dto.LocationOneResponseDto;
+import com.joobging.joobging.location.dto.LocationResponseDto;
+import com.joobging.joobging.location.service.LocationServiceImpl;
+import com.joobging.joobging.review.dto.ReviewResponseDto;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.List;
+
 @RestController
+@RequestMapping("/location")
+@RequiredArgsConstructor
 public class LocationController {
+
+    private final LocationServiceImpl locationService;
+
+    @GetMapping("/{location-id}")
+    public LocationResponseDto findReview(@PathVariable("location-id") Long locationId){
+        return locationService.findReview(locationId);
+    }
+
+    @GetMapping()
+    public List<LocationOneResponseDto> findAll(){
+        return locationService.findAll();
+    }
 }
