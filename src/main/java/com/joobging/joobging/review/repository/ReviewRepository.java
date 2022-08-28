e00b928d49f86cdd772909f47af3438c94e29b48
soluxsolux <dragon2000a@naver.com>
2022-08-28 10:36:33 +0900

    [add] ReviewRepository 추가

--- src/main/java/com/joobging/joobging/review/repository/ReviewRepository.java
+++ src/main/java/com/joobging/joobging/review/repository/ReviewRepository.java
@@ -1,6 +1,7 @@
 package com.joobging.joobging.review.repository;
 
 import com.joobging.joobging.review.domain.Review;
+import com.joobging.joobging.review.dto.ReviewResponseDto;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
@@ -13,4 +14,6 @@ public interface ReviewRepository extends JpaRepository<Review, Long> {
     @Query("select h from Review h ORDER BY h.id DESC")
     List<Review> findAllDesc();
 
+    @Query("select r from Review r where r.location.id = :locationId")
+    List<Review> findByReview_Id(Long locationId);
 }
