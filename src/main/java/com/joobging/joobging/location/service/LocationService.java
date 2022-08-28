5879d7c7e194efc2cb6917b49d5173c551ed41c4
soluxsolux <dragon2000a@naver.com>
2022-08-28 10:12:20 +0900

    [fix] location find sevice 수정

--- src/main/java/com/joobging/joobging/location/service/LocationService.java
+++ src/main/java/com/joobging/joobging/location/service/LocationService.java
@@ -1,4 +1,19 @@
 package com.joobging.joobging.location.service;
 
+import com.joobging.joobging.location.dto.LocationOneResponseDto;
+import com.joobging.joobging.location.dto.LocationResponseDto;
+import com.joobging.joobging.review.dto.ReviewResponseDto;
+
+import java.util.List;
+
 public interface LocationService {
+
+    // 각 location의 리뷰 모아 보기
+    LocationResponseDto findReview(Long locationId);
+
+
+    // location 전부 조회
+    List<LocationOneResponseDto> findAll();
+
+    // location 단건 조회
 }
--- src/main/java/com/joobging/joobging/location/service/LocationServiceImpl.java
+++ src/main/java/com/joobging/joobging/location/service/LocationServiceImpl.java
@@ -1,4 +1,48 @@
 package com.joobging.joobging.location.service;
 
-public class LocationServiceImpl {
+import com.joobging.joobging.location.domain.Location;
+import com.joobging.joobging.location.dto.LocationOneResponseDto;
+import com.joobging.joobging.location.dto.LocationResponseDto;
+import com.joobging.joobging.location.repository.LocationRepository;
+import com.joobging.joobging.review.domain.Review;
+import com.joobging.joobging.review.dto.ReviewResponseDto;
+import com.joobging.joobging.review.repository.ReviewRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
+import java.util.Optional;
+import java.util.stream.Collectors;
+
+@Service
+@RequiredArgsConstructor
+@Transactional(readOnly = true)
+public class LocationServiceImpl implements LocationService{
+
+    private final LocationRepository locationRepository;
+    private final ReviewRepository reviewRepository;
+
+    // 각 location 마다 리뷰 조회
+    public LocationResponseDto findReview(Long locationId){
+        Optional<Location> location = locationRepository.findById(locationId);
+        if(location.isEmpty()){
+            throw new IllegalArgumentException("location not found");
+        }
+        List<Review> reviewList = reviewRepository.findByReview_Id(locationId);
+        if(reviewList.isEmpty()){
+            throw new IllegalArgumentException("review not found");
+        }
+        List<ReviewResponseDto> reviewResponseDtoList = reviewList.stream().map(ReviewResponseDto::new).collect(Collectors.toList());
+        LocationResponseDto locationResponseDto = new LocationResponseDto(location.get());
+        locationResponseDto.setReviewResponseDtoList(reviewResponseDtoList);
+        return locationResponseDto;
+    }
+
+    // location 조회
+    public List<LocationOneResponseDto> findAll(){
+        List<Location> locationList = locationRepository.findAll();
+        return locationList.stream().map(LocationOneResponseDto::new).collect(Collectors.toList());
+    }
+
 }
